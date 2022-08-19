dc5dba3236cfb0521c4a5c1706949844e34edf3f
Fhellipe Valentin <fhellipereis@outlook.com>
2022-08-19 14:50:05 -0300

    hr-worker created

---
+++ hr-worker/src/main/java/io/github/fhellipevalentin/hrworker/HRWorkerApplication.java
@@ -0,0 +1,13 @@
+package io.github.fhellipevalentin.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HRWorkerApplication.class, args);
+    }
+
+}
---
+++ hr-worker/src/test/java/io/github/fhellipevalentin/hrworker/HRWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fhellipevalentin.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HRWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
