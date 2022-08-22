bbfd61a4b4d5d3c0dd32f74ea7923cea8dd8fe5d
Fhellipe Valentin <fhellipereis@outlook.com>
2022-08-22 17:33:49 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/io/github/fhellipevalentin/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/io/github/fhellipevalentin/hrworker/resources/WorkerResource.java
@@ -2,6 +2,9 @@ package io.github.fhellipevalentin.hrworker.resources;
 
 import io.github.fhellipevalentin.hrworker.entities.Worker;
 import io.github.fhellipevalentin.hrworker.repositories.WorkerRepository;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,6 +34,7 @@ public class WorkerResource {
 
     @GetMapping("{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("Port = " + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
