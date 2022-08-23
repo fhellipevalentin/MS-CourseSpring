544c4c00104d253a195d66715f98c7ef49650678
Fhellipe Valentin <fhellipereis@outlook.com>
2022-08-23 19:01:44 -0300

    Criando projeto hr-eureka-server

---
+++ hr-eureka-server/src/test/java/io/github/fhellipevalentin/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fhellipevalentin.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
