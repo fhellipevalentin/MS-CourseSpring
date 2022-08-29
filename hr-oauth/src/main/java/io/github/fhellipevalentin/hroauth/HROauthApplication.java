d71597a92723f5755000fc634ef4a46a28578ab5
Fhellipe Valentin <fhellipereis@outlook.com>
2022-08-29 16:10:18 -0300

    project hr-ouath created

---
+++ hr-oauth/src/main/java/io/github/fhellipevalentin/hroauth/HROauthApplication.java
@@ -0,0 +1,13 @@
+package io.github.fhellipevalentin.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HROauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HROauthApplication.class, args);
+	}
+
+}
---
+++ hr-oauth/src/test/java/io/github/fhellipevalentin/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fhellipevalentin.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
