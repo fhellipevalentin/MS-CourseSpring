41aafc31d173d2933a8ed15212a1fa2884480624
Fhellipe Valentin <fhellipereis@outlook.com>
2022-08-29 17:08:41 -0300

    UserFeignClient para OAuth

---
+++ hr-oauth/src/main/java/io/github/fhellipevalentin/hroauth/services/UserService.java
@@ -0,0 +1,27 @@
+package io.github.fhellipevalentin.hroauth.services;
+
+import io.github.fhellipevalentin.hroauth.entities.User;
+import io.github.fhellipevalentin.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    public User findByEmail(String email) {
+        User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null) {
+            logger.error("Email não encontrado" + email);
+            throw new IllegalArgumentException("Email não encontrado");
+        }
+        logger.info("Email encontrado: " + email);
+        return user;
+    }
+}
